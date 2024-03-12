fc6d050e0c53bfa72552d8ca653c3ee96f4f7a1e
connorGA <ronnoccap@gmail.com>
2024-03-11 21:59:04 -0700

    Variables basic concepts and types

---
+++ CS_basics/variables.py
@@ -0,0 +1,16 @@
+# BASIC CONCEPTS
+
+    # variables in Python are symbolic names that reference or point to data stored in memory
+    # variables are dynamically typed, meaning you dont need to declare their type before using them. The type is inferred from the assigned value
+
+# Types of Variables
+
+    # Numeric Types: Integers('int')
+    #                floating-point numbers('float')
+    #                complex numbers('complex')
+    # Sequence Types: Lists('list')
+    #                 tuples('tuple')
+    #                 strings('str')
+    # Mapping Type: Dictionary('dict') - Key value pairs are invaluable for mapping feature names to their values, storing model parameters or configuration settings             
+    # Boolean Type:('bool') - Represents two values, True or False
+    
