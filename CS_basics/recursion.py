2562406b1c9ae5822a793bcc1c5f28633cf84454
connorGA <ronnoccap@gmail.com>
2024-03-13 21:32:38 -0700

    basic principles of recursion

---
+++ CS_basics/recursion.py
@@ -0,0 +1,4 @@
+# Basic Principles of Recursion
+
+    # Base Case: This is the condition under which the recursion stops. The base case prevents the function from calling itself indefinitely, ensuring the recursion terminates
+    # Recursive Case: This is where the function calls itself, gradually appoaching the base case. Each recursive call should progress towards the base case to avoid infinite loops
