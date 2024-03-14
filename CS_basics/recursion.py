10bd04ce16348bfb11c6b95ae8f4edadd0cc083b
connorGA <ronnoccap@gmail.com>
2024-03-13 21:36:27 -0700

    advantages of recursion

--- CS_basics/recursion.py
+++ CS_basics/recursion.py
@@ -2,3 +2,9 @@
 
     # Base Case: This is the condition under which the recursion stops. The base case prevents the function from calling itself indefinitely, ensuring the recursion terminates
     # Recursive Case: This is where the function calls itself, gradually appoaching the base case. Each recursive call should progress towards the base case to avoid infinite loops
+
+# Advantages of Recursion
+
+    # Simplicity: Recursive solutions are often more straightforward and elegant for problems that involve repetitive processing of smaller, similar subproblems, such as tree traversals or factorial calculations
+    # Intuitiveness: Recursion can closely mirror the mathematical or conceptual definition of the problem, making the code more intuitive and easier to understand
+    # Power: Its particularly effective for solving complex algorithmic challenges that can be decomposed into smaller instances of the same problem, such as sorting algorithms(Like merge sort), searching algorithms(like Binary Search), and solving puzzles
